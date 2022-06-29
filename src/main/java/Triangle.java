8b8c7ae506329ee8f4d137c693b01738f7a910f3
Ärtiom Yeshchenko <eshchenko@gmail.com>
2022-06-29 23:46:29 +0400

    Initial commit

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,51 @@
+public class Triangle {
+    private double a, b, c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getSquare() {
+        double halfPerimeter = this.getPerimeter() / 2;
+        return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
+    }
+
+    public double getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
+    }
+}
